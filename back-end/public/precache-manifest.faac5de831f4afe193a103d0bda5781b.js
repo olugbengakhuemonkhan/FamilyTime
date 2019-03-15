f26e0345377f41cad41c9065fd3be931724e68dc
Akhuemonkhan Olugbenga <gbengaloveeyesimages@gmail.com>
2019-03-15 11:38:09 -0400

    build

---
+++ back-end/public/precache-manifest.faac5de831f4afe193a103d0bda5781b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "025c2967a00822ba5ee4fac87c4b5e55",
+    "url": "/static/media/logo.025c2967.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b3cff4fc84de0111cdae",
+    "url": "/static/js/main.b3cff4fc.chunk.js"
+  },
+  {
+    "revision": "521c04ee8ad65471b876",
+    "url": "/static/js/2.521c04ee.chunk.js"
+  },
+  {
+    "revision": "b3cff4fc84de0111cdae",
+    "url": "/static/css/main.22f932ae.chunk.css"
+  },
+  {
+    "revision": "521c04ee8ad65471b876",
+    "url": "/static/css/2.125692ac.chunk.css"
+  },
+  {
+    "revision": "9852d61ca462b2270f0097f6b6665a3c",
+    "url": "/index.html"
+  }
+];
