65f20dbc465165da607ee84d0a30d78e311ef72d
Akhuemonkhan Olugbenga <gbengaloveeyesimages@gmail.com>
2019-06-10 10:06:54 -0400

    Update

--- front-end/src/NavBar.js
+++ front-end/src/NavBar.js
@@ -26,26 +26,21 @@ class NavBar extends Component {
 
             <ul id="nav-mobile" className="left hide-on-med-and-down">
             <li className= "col s1"><Link onClick={()=>{this.setActiveLink('/')}} to="/">
-              <span className={this.state.activeLink=='/' ? 'active' : ""}>Home</span></Link></li>
+              <span className={this.state.activeLink==='/' ? 'active' : ""}>Home</span></Link></li>
               <li className= "col s1"><Link onClick={()=>{this.setActiveLink('/about')}} to="/about">
-                <span className={this.state.activeLink=='/about' ? 'active' : ""}>About</span></Link></li>
+                <span className={this.state.activeLink==='/about' ? 'active' : ""}>About</span></Link></li>
               <li className= "col s1"><Link onClick={()=>{this.setActiveLink('/weather')}} to="/weather">
-                <span className={this.state.activeLink=='/weather' ? 'active' : ""}>Weather</span></Link></li>
+                <span className={this.state.activeLink==='/weather' ? 'active' : ""}>Weather</span></Link></li>
               <li className= "col s1"><Link onClick={()=>{this.setActiveLink('/myCalendar')}} to="/myCalendar">
-                <span className={this.state.activeLink=='/myCalendar' ? 'active' : ""}>MyCalendar</span></Link></li>
+                <span className={this.state.activeLink==='/myCalendar' ? 'active' : ""}>MyCalendar</span></Link></li>
               <li className= "col s1"><Link onClick={()=>{this.setActiveLink('/movies')}} to="/movies">
-                <span className={this.state.activeLink=='/movies' ? 'active' : ""}>Movies</span></Link></li>
+                <span className={this.state.activeLink==='/movies' ? 'active' : ""}>Movies</span></Link></li>
               <li className= "col s1"><Link onClick={()=>{this.setActiveLink('/event')}} to="/event">
-                <span className={this.state.activeLink=='/event' ? 'active' : ""}>Event</span></Link></li>
+                <span className={this.state.activeLink==='/event' ? 'active' : ""}>Event</span></Link></li>
             </ul>
           </div>
         </nav>
       </div>
-
-
-
-
-
     )
   }
 }
